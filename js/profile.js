9bfc69d3e45373f590bbcd00f06b6322ad423141
Ruhid Memmedzade <114422609+mruhid@users.noreply.github.com>
2024-12-21 14:00:46 +0400

    Update profile.js

--- js/profile.js
+++ js/profile.js
@@ -140,12 +140,14 @@ async function updateProfile() {
     data.presentations.forEach((presentation) => {
       const presentationLink = document.createElement("a");
       presentationLink.classList.add("presentation");
-      presentationLink.href = presentation.link;
+      const src1=`${basePath}/presentation/components/${presentation.link}`
+      presentationLink.href =src1;
       presentationLink.title = presentation.altText;
       presentationLink.target = "_blank";
 
       const img = document.createElement("img");
-      img.src = presentation.imageSrc;
+      const src2=`${basePath}/presentation/components/${presentation.imageSrc}`
+      img.src = src2;
       img.alt = presentation.altText;
       presentationLink.appendChild(img);
 
